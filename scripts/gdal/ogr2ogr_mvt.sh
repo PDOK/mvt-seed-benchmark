9efbb93f4a4212265d79e276c1ee3f31039c6256
Roel van den Berg <roeland.van.den.berg@ordina.nl>
2019-09-09 16:31:03 +0200

    improve tippecanoe benchmark

--- scripts/gdal/ogr2ogr_mvt.sh
+++ scripts/gdal/ogr2ogr_mvt.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
-echo "Creating MVT for $1 with "
-ogr2ogr -f MVT test -a_srs EPSG:28992 /data/$1 -fieldTypeToString StringList,IntegerList,Date --config GML_SKIP_RESOLVE_ELEMS HUGE --config GML_SKIP_RESOLVE_ELEMS NONE --config GML_EXPOSE_FID NO --config GML_EXPOSE_GML_ID NO --config GDAL_NUM_THREADS 0 -dsco MINZOOM=1 -dsco MAXZOOM=11 -dsco TILING_SCHEME=EPSG:28992,-285401.92,903402.0,880803.84
+PLAN_ID="NL.IMRO.0437.BPDRHNoodoverloop-ON01"
+mkdir -p "data/result/gdal"
+FILENAME="data/simplified/$PLAN_ID-simplified.gml"
+for FILENAME in $(ls data/simplified/*-simplified.gml)
+  do
+    BASENAME=$(basename $FILENAME)
+    PLAN_ID=${BASENAME%-*}
+
+    echo "Creating MVT for $1 with "
+
+    # Log step, PlanID, time spent, cpu, Memory usage in bytes
+    LOG_FORMAT="${PLAN_ID},%E,%P,%K"
+
+    RESULT_DIR="data/result/gdal/${PLAN_ID}"
+
+    STEP="Generate MVTs GDAL"
+    echo "$STEP"
+    /usr/bin/time --format="$STEP,$LOG_FORMAT" -o log/gdal_benchmark.log --append \
+      docker-compose run --rm -u "$UID:$UID" gdal \
+      ogr2ogr -f MVT \
+      "/$RESULT_DIR" \
+      -a_srs EPSG:28992 \
+      "/data/simplified/$PLAN_ID-simplified.gml" \
+      -fieldTypeToString StringList,IntegerList,Date \
+      --config GML_SKIP_RESOLVE_ELEMS HUGE \
+      --config GML_SKIP_RESOLVE_ELEMS NONE \
+      --config GML_EXPOSE_FID NO \
+      --config GML_EXPOSE_GML_ID NO \
+      --config GDAL_NUM_THREADS 0 \
+      -dsco MINZOOM=1 \
+      -dsco MAXZOOM=11 \
+      -dsco TILING_SCHEME=EPSG:28992,-285401.92,903402.0,880803.84
+done
--- scripts/tippecanoe/tippecanoe_generate_mvt.sh
+++ scripts/tippecanoe/tippecanoe_generate_mvt.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-PLAN_ID="NL.IMRO.0437.BPDRHNoodoverloop-ON01"
 
 ## PlanID, Log step, time spent, real time, user time, system time, cpu, Memory usage in bytes, inputs, outputs, max, swaps, major, minor
 # VERBOSE_LOG_FORMAT="${PLAN_ID},%E,%e,%U,%s,%P,%K,%I,%O,%D,%M,%W,%F,%R"
@@ -8,26 +7,42 @@ for FILENAME in $(ls data/simplified/*-simplified.gml)
   do
     BASENAME=$(basename $FILENAME)
     PLAN_ID=${BASENAME%-*}
-    # PlanID, Log step, time spent, cpu, Memory usage in bytes
+    # Log step, PlanID, time spent, cpu, Memory usage in bytes
     LOG_FORMAT="${PLAN_ID},%E,%P,%K"
 
     STEP="Generate GeoJSON"
     echo "$STEP"
     /usr/bin/time --format="$STEP,$LOG_FORMAT" -o log/tippecanoe_benchmark.log --append \
+    docker-compose run --rm -u "$UID:$UID" gdal \
       ogr2ogr \
         -f GeoJSON \
         -a_srs EPSG:28992 \
+        -a_srs EPSG:4326 \
         -fieldTypeToString StringList,IntegerList,Date \
-        "data/$PLAN_ID.json" \
-        "data/simplified/$PLAN_ID-simplified.gml" \
+        "/data/$PLAN_ID.json" \
+        "/data/simplified/$PLAN_ID-simplified.gml" \
         Planobject
 
-    RESULT_DIR="data/result/${PLAN_ID}"
+    RESULT_DIR="data/result/tippecanoe/${PLAN_ID}"
 
-    mkdir $RESULT_DIR
+    mkdir -p "$RESULT_DIR"
 
     STEP="Run tippecanoe"
     echo "$STEP"
     /usr/bin/time --format="$STEP,$LOG_FORMAT" -o log/tippecanoe_benchmark.log --append \
-      docker-compose run --rm tippecanoe $PLAN_ID $RESULT_DIR
+      docker-compose run --rm -u "$UID:$UID" tippecanoe \
+        tippecanoe \
+          --name="$PLAN_ID" \
+          --output-to-directory="/${RESULT_DIR}" \
+          --minimum-zoom=1 \
+          --maximum-zoom=11 \
+          --full-detail=11 \
+          --low-detail=11 \
+          --minimum-detail=7 \
+          --drop-densest-as-needed \
+          --detect-shared-borders \
+          --buffer=5 \
+          "/data/${PLAN_ID}.json"
+
+    rm "/data/${PLAN_ID}.json"
 done
