ff597a1cc80b94d59a93f07dd659c83ed8b69154
Anton Bakker <anton.bakker@kadaster.nl>
2019-09-10 17:18:09 +0200

    verbeter dingen

--- scripts/runall.sh
+++ scripts/runall.sh
@@ -1,9 +1,12 @@
+#!/usr/bin/env bash
+
+CURRENT_DIR="${0%/*}"
 for i in {1..5}
 do
   UUID=$(uuidgen)
   RUN_IDS="$UUID,$i"
   echo "### Running iteration $i with uuid $UUID ###"
-#  ./scripts/t-rex/seed.sh $RUN_IDS
-  ./scripts/tippecanoe/tippecanoe_mvt.sh $RUN_IDS
-#  ./scripts/gdal/ogr2ogr_mvt.sh $RUN_IDS
+  $CURRENT_DIR/t-rex/trex_mvt.sh $RUN_IDS
+  $CURRENT_DIR/tippecanoe/tippecanoe_mvt.sh $RUN_IDS
+  $CURRENT_DIR/gdal/ogr2ogr_mvt.sh $RUN_IDS
 done
--- scripts/util.sh
+++ scripts/util.sh
@@ -9,12 +9,10 @@ log_filecount_and_dirsize() {
   RUN_IDS=$5
   LOG_ROW=""
   LOGFILE_NAME="$BASE_DIR/log/${BENCHMARK_TYPE}_counts.log"
-
   for ((ZOOM_LEVEL=MIN_ZOOM; ZOOM_LEVEL<MAX_ZOOM; ZOOM_LEVEL++))
   do
     COUNT_DIR="$BASE_DIR/data/result/$BENCHMARK_TYPE/$PLAN_ID/$ZOOM_LEVEL"
     LOG_ROW+=",$(du -s "$COUNT_DIR" | cut -f1),$(find "$COUNT_DIR" -type f | wc -l)"
   done
-
   echo "$RUN_IDS,$PLAN_ID,$(date +%FT%T%Z)$LOG_ROW" >> $LOGFILE_NAME
 }
